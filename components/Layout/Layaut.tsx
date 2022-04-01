9efd3431ed46639663b39fbab5ef9727cf431913
Ing. Eduardo Pinedo <33038866+Eduyy24@users.noreply.github.com>
2022-03-31 23:05:50 -0500

    optimitation for using all screen

--- components/Layout/Layaut.tsx
+++ components/Layout/Layaut.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from "react"
-import style from './Layout.module.css';
 import { Container } from "../Container/Container";
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
@@ -11,11 +10,9 @@ export const Layout: FunctionComponent<LayoutProps> = ({children}) => {
   return (
     <>
       <Header />
-        <div className={style.container}>
-          <Container>
-            {children}
-          </Container>
-        </div>
+        <Container>
+          {children}
+        </Container>
       <Footer />
     </>
   )
